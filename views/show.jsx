dea369456671a391bde58360636a44f5959f28f5
Donald Evans <devans08123@gmail.com>
2023-05-11 20:34:22 -0400

    adding bakers

--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Show({ bread, index }) {
+function Show({ bread }) {
     
     return (
         <Default>
@@ -14,11 +14,12 @@ function Show({ bread, index }) {
                 have gulten
             </p>
             <img src={bread.image} alt={bread.name} />
-            <a href={`/breads/${index}/edit`}>
+            <p>{bread.getBakedBy()}</p>
+            <a href={`/breads/${bread.id}/edit`}>
                 <button>Edit</button>
             </a>
             <li>
-            <form action={`/breads/${index}?_method=DELETE`} method='POST'>
+            <form action={`/breads/${bread.id}?_method=DELETE`} method='POST'>
                 <input type='submit' value='DELETE' />
             </form>
                 <a href={'/breads'} >Go Home</a>
