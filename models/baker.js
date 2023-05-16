d3eb6d4e76cedac616dc814f80d309e9d4ccf1fa
Donald Evans <devans08123@gmail.com>
2023-05-16 18:47:17 -0400

    deleing bakers

--- models/baker.js
+++ models/baker.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const Bread = require('./bread')
+
 
 const bakerSchema = new mongoose.Schema({
     name: {
@@ -14,6 +16,10 @@ const bakerSchema = new mongoose.Schema({
 }, {
     toJSON: { virtuals: true }
 }) 
+
+bakerSchema.post('findOneAndDelete', async function() {
+    await Bread.deleteMany({baker: this._conditions._id})
+})
 
 
 bakerSchema.virtual('breads', {
